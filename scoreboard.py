23d321e8b05ccd70c219848b4af8930472523cc4
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 11:53:28 +0500

    Add Scoreboard class with level and game over functionality

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,24 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.printLevel()
+
+    def printLevel(self):
+        self.reset()
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 280)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def gameOver(self):
+        self.reset()
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
