73ac54b8d343864635b27a8623e95607afd6dbdf
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 11:31:24 +0500

    Add car creation and movement in game loop

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ screen.tracer(0)
 
 # Initialize Objects
 player = Player()
+carManager = CarManager()
 
 # Player Controls
 screen.listen()
 screen.onkey(player.up, "Up")
 # Game Loop
 game_is_on = True
+count = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if count % 6 == 0:
+        carManager.createCar()
+    carManager.moveCars()
+    count += 1
 
 screen.exitonclick()
