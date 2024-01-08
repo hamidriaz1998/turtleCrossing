68c92cb5dddbbe3c37e1e820610e318c26322f99
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 11:24:13 +0500

    Add car creation and movement functionality

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.allCars = []
+
+    def createCar(self):
+        car = Turtle("square")
+        car.penup()
+        car.color(random.choice(COLORS))
+        car.setheading(180)
+        car.goto(MOVE_INCREMENT, random.randint(-250, 250))
+        self.allCars.append(car)
+
+    def moveCars(self):
+        for car in self.allCars:
+            car.forward(MOVE_INCREMENT)
+
+    # def increaseSpeed(self):
+    #     MOVE_INCREMENT += 1
