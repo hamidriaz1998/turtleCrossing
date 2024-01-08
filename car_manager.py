a589753345901e401a12bd6516deb2ea47c2b14c
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 11:31:14 +0500

    Update car size and starting position

--- car_manager.py
+++ car_manager.py
@@ -14,13 +14,14 @@ class CarManager:
         car = Turtle("square")
         car.penup()
         car.color(random.choice(COLORS))
+        car.shapesize(stretch_len=2, stretch_wid=1)
         car.setheading(180)
-        car.goto(MOVE_INCREMENT, random.randint(-250, 250))
+        car.goto(300, random.randint(-250, 250))
         self.allCars.append(car)
 
     def moveCars(self):
         for car in self.allCars:
-            car.forward(MOVE_INCREMENT)
+            car.forward(STARTING_MOVE_DISTANCE)
 
     # def increaseSpeed(self):
     #     MOVE_INCREMENT += 1
